e2638e554fd756e55f008960c7971c417bdf1679
ndhuru <144291089+ndhuru@users.noreply.github.com>
2024-02-19 13:16:43 -0500

    Update lane detect.py

--- lane detect.py
+++ lane detect.py
@@ -26,7 +26,7 @@ def lineDetection(image):
     cv.rectangle(image, (roi_tl_x, roi_tl_y), (roi_br_x, roi_br_y), (255, 0, 0), 5)
 
     # Creates the hough lines used for the line detection
-    lines = cv.HoughLinesP(mask, 1, np.pi / 180, threshold=10, minLineLength=30, maxLineGap=5)
+    lines = cv.HoughLinesP(mask, 1, np.pi / 180, threshold=20, minLineLength=30, maxLineGap=5)
 
     return lines
 
